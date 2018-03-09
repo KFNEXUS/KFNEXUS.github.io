0eeef378e5fc6ce1d3adad85667b5de7f4867502
CrazyVulcan <butters1115@gmail.com>
2018-03-09 11:05:54 -0500

    A number of small edits

--- src/js/common/utopia-card-rules.js
+++ src/js/common/utopia-card-rules.js
@@ -3462,6 +3462,9 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 		//Systems Upgrade
 		"tech:systems_upgrade_71998p": {
 			type: "question",
+			factionPenalty: function(upgrade, ship, fleet) {
+				return ship && $factions.hasFaction( ship, "bajoran", ship, fleet ) ? 0 : 1 && $factions.hasFaction( ship, "vulcan", ship, fleet ) ? 0 : 1;
+			},
 			isSlotCompatible: function(slotTypes) {
 				return $.inArray( "tech", slotTypes ) >= 0 || $.inArray( "weapon", slotTypes ) >= 0 || $.inArray( "crew", slotTypes ) >= 0;
 			},
@@ -3481,7 +3484,7 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 			},
 			canEquip: onePerShip("Systems Upgrade"),
 			canEquipFaction: function(upgrade,ship,fleet) {
-				return $factions.hasFaction(ship,"federation", ship, fleet);
+				return ($factions.hasFaction(ship,"federation", ship, fleet) || $factions.hasFaction(ship,"bajoran", ship, fleet) || $factions.hasFaction(ship,"vulcan", ship, fleet) );
 			}
 		},
 		//Type 8 Phaser Array
@@ -7259,7 +7262,14 @@ module.factory( "cardRules", [ "$filter", "$factions", function($filter, $factio
 		//Shield Adaption
 		"tech:shield_adaptation_72013wp":{
 			//Hull is equal or greater then 4, needs to work with fleet commander
+			canEquip: function(upgrade,ship,fleet) {
+				return onePerShip("Reinforced Shields") && ship.hull >= 4;
+			}
 		},
+		"tech:belannas_codes_72013wp":{
+			factionPenalty: function(upgrade, ship, fleet) {
+				return ship && $factions.hasFaction( ship, "ferengi", ship, fleet ) ? 0 : 1 && $factions.hasFaction( ship, "kazon", ship, fleet ) ? 0 : 1 && $factions.hasFaction( ship, "xindi", ship, fleet ) ? 0 : 1;
+			}},
 
 	//Prototype 02 :72014wp
 		"ship:prototype_02_72014wp": {
